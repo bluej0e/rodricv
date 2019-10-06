aadc6fc2ecd6429475d97dadfb4dc162489552f8
Rodrigo Viola <rv.rodrigo.viola@gmail.com>
2019-10-06 20:44:56 +0200

    no message

--- php/mail_handler.php
+++ php/mail_handler.php
@@ -54,13 +54,4 @@ function SendEmail($message, $subject, $from, $to) {
 }
 
 
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
-
-
 ?>
