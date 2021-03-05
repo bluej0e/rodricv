f036e755ec45f54a3fcb1844bc0a274b2cfe8937
Rodrigo Viola <rv.rodrigo.viola@gmail.com>
2021-03-05 17:54:12 -0300

    added lang support

---
+++ js/langs.js
@@ -0,0 +1,310 @@
+const langEng = {
+  lang: "en",
+  opLang: "Español",
+  title: "Digital Marketer &amp; Web Developer",
+  navBar: {
+    home: "Home",
+    about: "About",
+    education: "Education",
+    skills: "Skills",
+    experience: "Experience",
+    portfolio: "Portfolio",
+    interests: "Interests",
+    contact: "Contact"
+  },
+  about: {
+    main: "Hello! I'm Rodrigo, I'm currently based in Montevideo working as a freelancer. I’ve had the opportunity to work with some of the largest iGaming and retail companies in the world. Having an entrepreneurial spirit I run a series of personal projects and am always looking for the next challenge. In my life I've had the chance to live in Canada, Uruguay and Spain for many years which enabled me to master both English and Spanish and also study and work in these countries in the fields of client management, marketing and web development. Currently I'm looking for my next challenge.",
+    download: "Download CV",
+    contact: "Contact Me",
+  },
+  education: {
+    title: "Education",
+    sec1: {
+      title: "High School",
+      small: "Abbey Park HS",
+      location: "Oakville, Canada",
+      p: "I completed my high school degree from Abbey Park High School. I successfully completed all the credits without any fallout. This is where my professional journey began."
+    },
+    sec2: {
+      title: "Advertising & Design",
+      small: "Sheridan College",
+      date:"2007 - 2009",
+      location: "Toronto, Canada",
+      p: "I entered the Advertising and Design program at Sheridan College after high school. I went into this program with a design perspective, but what it really taught me was how much I like the business side of advertising more than the design. After two years I decided to focus on the marketing side of things."
+    },
+    sec3: {
+      title: "Advertising & Marketing Communnications",
+      small: "Algonquin College",
+      date:"2009 - 2012",
+      location: "Ottawa, Canada",
+      p: "Quickly after finishing at Sheridan I enrolled in an Advertising and Marketing Communications program. Here I was able to focus more on the business side of advertising, as well as learn about different facets of the business world."
+    },
+    sec4: {
+      title: "Marketing Analyst",
+      small: "ORT University",
+      date:"2015 - 2017",
+      location: "Montevideo, Uruguay",
+      p: "In early 2015 I moved to Montevideo where I took a two year Marketing analysis program. I used this opportunity to improve my analytical skills, as well as my spanish. During my time here I focused more on the digital side of marketing."
+    },
+    sec5: {
+      title: "School of Life",
+      p: "Currently I am not enrolled in any academic programs, but as we all know it is key to stay up to date with technologies, information and new trends, so I take a lot of digital classes, and am constantly trying to improve myself in any way possible. Lately I have been taking a lot of programming classes to better my web development skills as well as general marketing and design classes."
+    },
+  },
+  skills: {
+    title: "Skills",
+    col1: {
+      t: "Professional",
+      s1: "HTML5",
+      s2: "CSS3",
+      s3: "Javascript",
+      s4: "SEM",
+      s5: "SEO"
+    },
+    col2: {
+      t: "Personal",
+      s1: "Communication",
+      s2: "Teamwork",
+      s3: "Creativity",
+      s4: "Detailed",
+      s5: "Adaptive"
+    },
+    col3: {
+      t: "Software",
+      s1: "Illustrator",
+      s2: "inDesign",
+      s3: "Photoshop",
+      s4: "Git",
+      s5: "Adobe XD"
+    },
+  },
+  experience: {
+    title: "Experience",
+    sec1: {
+      title: "Apple Sales Assosiate",
+      company: "Apple Inc.",
+      location: "Ottawa, Canada",
+      text: "In 2012 I started working at <i>Apple</i> in the Ottawa store. I was primarily tasked as a sales person, which taught me some useful customer service skills. I also took part in the maintenance and visuals teams. In 2013 I helped open the second store in Ottawa, where I remained. <i>Apple</i> showed me what it means to be in a positive work environment.",
+    },
+    sec2: {
+      title: "Consultant",
+      company: "Trafilea",
+      location: "Montevideo, Uruguay",
+      text: "I worked as a consultant for (at the time) a small digital marketing agency <i>Trafilea</i>. I was tasked with coordinating teams of designers, programmers, and copywriters. As well as innovating and streamlining their customer acquisition process. This is when I became more involved in the production process of digital content.",
+    },
+    sec3: {
+      title: "Product Manager",
+      company: "LatamGaming",
+      location: "Montevideo, Uruguay",
+      text: "For about two years I worked alongside iGaming affiliate network operator <i>LatamGaming</i> where I was tasked with project management, graphic design, SEO/SEM, web development, growth hacking and digital marketing. I oversaw and operated the development of an 80+ website iGaming affiliate network which spanned all of Latin America.",
+    },
+    sec4: {
+      title: "Owner / Freelance",
+      company: "1UP.uy",
+      location: "Remote Work",
+      text: "For roughly two years I worked as a freelancer under my personal brand <i>1UP.uy</i>. I have a series of personal projects that I still operate on the side. During my time operating <i>1UP</i> I worked with leading iGaming companies in Sweden and Malta doing things such as market research, growth hacking, and the development of new markets. I also did some work with smaller companies which may involve a simple website or logo. In contrast; I’ve also done things like co-organized <i>the Jump! Event</i> in Montevideo, Uruguay.",
+    },
+    sec5: {
+      title: "Assosiate / CTO",
+      company: "Power Global Innovation",
+      location: "Barcelona, Spain",
+      text: "In late 2019 I met my soon-to-be associate. With him we worked two projects simultaneously: <i>RankingOnline.es</i> – A 360 digital agency & <i>TallerDelPatinete.es</i> – An eCommerce / distribution website focused in the personal electric mobility market. These projects were a two man operation, where in which we both took on many different tasks. In both of these projects I was in charge of developing the digital content. In the agency I was the designer, developer and technical guy. I also had 2-3 interns working with me at all times. I created the sales pitches, the general ideas and directed the designers and developers to achieve the best result for the clients. As for the <i>TallerDelPatinete</i> project, I was in charge of the design, inventory management, SEM, maintenance of the websites and development of the network (amongst many other tasks). In the time we worked on these projects I became very versatile and self sufficient, I learned a lot about how to run a small business, international business and sales. Unfortunately due to the 2020 financial climate in Spain, my associate decided to part from the project. We still run the marketing agency; but as freelancers.",
+    },
+  },
+  portfolio: {
+    title: "Portfolio",
+  },
+  interests: {
+    title: "Interests",
+    p: "First of all, I love games. Be it video games or board games, cooperative games or competitive ones, when it comes to games, I’m the first one on board. I try to spend as much time outside as possible, always on the lookout trying to find a good spot in the city to have a cup of coffee, or some hidden views to take a great shot, or simply a quiet corner to just relax. Lately I've been getting into 3D animation, being a quite tech savvy person I’ve always admired those who are skilled at 3D, I think of it as the pinnacle of digital art. As for sports, I’m more of a solo guy; I like biking, swimming and yoga... I’ve never been good at team sports. I also enjoy traveling, meeting new people and immersing myself in their cultures. I find it to be the best way to really learn about different places.",
+    i1: "Exploring",
+    i2: "Gaming",
+    i3: "Nature",
+    i4: "Biking",
+    i5: "Traveling",
+    i6: "Photography"
+  },
+  contact:{
+    title:"Contact",
+    name: "Name",
+    sub: "Subject",
+    email: "Email",
+    msg: "Message",
+    send: "Send"
+  }
+};
+
+const langEsp = {
+  lang: "es",
+  opLang: "English",
+  title: "Marketing Digital &amp; Desarrollo Web",
+  navBar: {
+    home: "Home",
+    about: "About",
+    education: "Education",
+    skills: "Skills",
+    experience: "Experience",
+    portfolio: "Portfolio",
+    interests: "Interests",
+    contact: "Contact"
+  },
+  about: {
+    main: "Hello! I'm Rodrigo, I'm currently based in Montevideo working as a freelancer. I’ve had the opportunity to work with some of the largest iGaming and retail companies in the world. Having an entrepreneurial spirit I run a series of personal projects and am always looking for the next challenge. In my life I've had the chance to live in Canada and Uruguay for many years which enabled me to master both English and Spanish and also study and work in both countries in the fields of client management, marketing and web development. Currently I'm looking for my next challenge.",
+    download: "Download CV",
+    contact: "Contact Me",
+  },
+  education: {
+    title: "Education",
+    sec1: {
+      title: "High School",
+      small: "Abbey Park HS",
+      location: "Oakville, Canada",
+      p: "I completed my high school degree from Abbey Park High School. I successfully completed all the credits without any fallout. This is where my professional journey began."
+    },
+    sec2: {
+      title: "Advertising & Design",
+      small: "Sheridan College",
+      date:"2007 - 2009",
+      location: "Toronto, Canada",
+      p: "I entered the Advertising and Design program at Sheridan College after high school. I went into this program with a design perspective, but what it really taught me was how much I like the business side of advertising more than the design. After two years I decided to focus on the marketing side of things."
+    },
+    sec3: {
+      title: "Advertising & Marketing Communnications",
+      small: "Algonquin College",
+      date:"2009 - 2012",
+      location: "Ottawa, Canada",
+      p: "Quickly after finishing at Sheridan I enrolled in an Advertising and Marketing Communications program. Here I was able to focus more on the business side of advertising, as well as learn about different facets of the business world."
+    },
+    sec4: {
+      title: "Marketing Analyst",
+      small: "ORT University",
+      date:"2015 - 2017",
+      location: "Montevideo, Uruguay",
+      p: "In early 2015 I moved to Montevideo where I took a two year Marketing analysis program. I used this opportunity to improve my analytical skills, as well as my spanish. During my time here I focused more on the digital side of marketing."
+    },
+    sec5: {
+      title: "School of Life",
+      p: "Currently I am not enrolled in any academic programs, but as we all know it is key to stay up to date with technologies, information and new trends, so I take a lot of digital classes, and am constantly trying to improve myself in any way possible. Lately I have been taking a lot of programming classes to better my web development skills as well as general marketing and design classes."
+    },
+  },
+  skills: {
+    title: "Skills",
+    col1: {
+      t: "Professional",
+      s1: "HTML5",
+      s2: "CSS3",
+      s3: "Javascript",
+      s4: "SEM",
+      s5: "SEO"
+    },
+    col2: {
+      t: "Personal",
+      s1: "Communication",
+      s2: "Teamwork",
+      s3: "Creativity",
+      s4: "Detailed",
+      s5: "Adaptive"
+    },
+    col3: {
+      t: "Software",
+      s1: "Illustrator",
+      s2: "inDesign",
+      s3: "Photoshop",
+      s4: "Git",
+      s5: "Adobe XD"
+    },
+  },
+  experience: {
+    title: "Experience",
+    sec1: {
+      title: "Apple Sales Assosiate",
+      company: "Apple Inc.",
+      location: "Ottawa, Canada",
+      text: "In 2012 I started working at <i>Apple</i> in the Ottawa store. I was primarily tasked as a sales person, which taught me some useful customer service skills. I also took part in the maintenance and visuals teams. In 2013 I helped open the second store in Ottawa, where I remained. <i>Apple</i> showed me what it means to be in a positive work environment.",
+    },
+    sec2: {
+      title: "Consultant",
+      company: "Trafilea",
+      location: "Montevideo, Uruguay",
+      text: "I worked as a consultant for (at the time) a small digital marketing agency <i>Trafilea</i>. I was tasked with coordinating teams of designers, programmers, and copywriters. As well as innovating and streamlining their customer acquisition process. This is when I became more involved in the production process of digital content.",
+    },
+    sec3: {
+      title: "Product Manager",
+      company: "LatamGaming",
+      location: "Montevideo, Uruguay",
+      text: "For about two years I worked alongside iGaming affiliate network operator <i>LatamGaming</i> where I was tasked with project management, graphic design, SEO/SEM, web development, growth hacking and digital marketing. I oversaw and operated the development of an 80+ website iGaming affiliate network which spanned all of Latin America.",
+    },
+    sec4: {
+      title: "Owner / Freelance",
+      company: "1UP.uy",
+      location: "Remote Work",
+      text: "For roughly two years I worked as a freelancer under my personal brand <i>1UP.uy</i>. I have a series of personal projects that I still operate on the side. During my time operating <i>1UP</i> I worked with leading iGaming companies in Sweden and Malta doing things such as market research, growth hacking, and the development of new markets. I also did some work with smaller companies which may involve a simple website or logo. In contrast; I’ve also done things like co-organized <i>the Jump! Event</i> in Montevideo, Uruguay.",
+    },
+    sec5: {
+      title: "Assosiate / CTO",
+      company: "Power Global Innovation",
+      location: "Barcelona, Spain",
+      text: "In late 2019 I met my soon-to-be associate. With him we worked two projects simultaneously: <i>RankingOnline.es</i> – A 360 digital agency & <i>TallerDelPatinete.es</i> – An eCommerce / distribution website focused in the personal electric mobility market. These projects were a two man operation, where in which we both took on many different tasks. In both of these projects I was in charge of developing the digital content. In the agency I was the designer, developer and technical guy. I also had 2-3 interns working with me at all times. I created the sales pitches, the general ideas and directed the designers and developers to achieve the best result for the clients. As for the <i>TallerDelPatinete</i> project, I was in charge of the design, inventory management, SEM, maintenance of the websites and development of the network (amongst many other tasks). In the time we worked on these projects I became very versatile and self sufficient, I learned a lot about how to run a small business, international business and sales. Unfortunately due to the 2020 financial climate in Spain, my associate decided to part from the project. We still run the marketing agency; but as freelancers.",
+    },
+  },
+  portfolio: {
+    title: "Portfolio",
+  },
+  interests: {
+    title: "Interests",
+    p: "First of all, I love games. Be it video games or board games, cooperative games or competitive ones, when it comes to games, I’m the first one on board. I try to spend as much time outside as possible, always on the lookout trying to find a good spot in the city to have a cup of coffee, or some hidden views to take a great shot, or simply a quiet corner to just relax. Lately I've been getting into 3D animation, being a quite tech savvy person I’ve always admired those who are skilled at 3D, I think of it as the pinnacle of digital art. As for sports, I’m more of a solo guy; I like biking, swimming and yoga... I’ve never been good at team sports. I also enjoy traveling, meeting new people and immersing myself in their cultures. I find it to be the best way to really learn about different places.",
+    i1: "Exploring",
+    i2: "Gaming",
+    i3: "Nature",
+    i4: "Biking",
+    i5: "Traveling",
+    i6: "Movies"
+  },
+  contact:{
+    title:"Contact",
+    name: "Name",
+    sub: "Subject",
+    email: "Email",
+    msg: "Message",
+    send: "Send"
+  }
+};
+
+var langBox = document.querySelector('.langBox');
+
+langBox.addEventListener('click', chosenLang());
+
+function textFillEn(e){
+  var textStuff = e.getAttribute('data-lang').split(".");
+  var tempObj = langEng;
+
+  for (var i = 0; i < textStuff.length; i++) {
+    tempObj = tempObj[textStuff[i]]
+  }
+  e.innerHTML = tempObj;
+};
+
+function textFillEs(e){
+  var textStuff = e.getAttribute('data-lang').split(".");
+  var tempObj = langEsp;
+
+  for (var i = 0; i < textStuff.length; i++) {
+    tempObj = tempObj[textStuff[i]]
+  }
+  e.innerHTML = tempObj;
+};
+
+function chosenLang(){
+  var curLang = langBox.getAttribute('lang');
+  var elemsToChange = document.querySelectorAll("[data-lang]");
+
+  if(curLang == 'en'){
+    console.log('english');
+    elemsToChange.forEach(textFillEn);
+    langBox.lang = 'es';
+  }else if (curLang == 'es') {
+    console.log('spanish');
+    elemsToChange.forEach(textFillEs);
+    langBox.lang = 'en';
+  }
+};
